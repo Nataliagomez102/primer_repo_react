13e4b84bfda8ce91e28386c3b39cec9c9f5709f1
nataliagomez102 <nataliagomez102@gmail.com>
2023-02-02 22:40:57 -0500

    formulario

--- src/app.jsx
+++ src/app.jsx
@@ -5,8 +5,12 @@ import foto from './assets/img/foto.jpeg'
 
 
 
-export function App() {
 
+export function App() {
+  const [nombre,setNombre]= useState ()
+  console.log("este es el nombre",nombre);
+  setNombre("natalia")
+  console.log("este es el nombre",nombre);
   return (
     <>
       <div className='contenedor'>
@@ -20,14 +24,29 @@ export function App() {
                   </div>
         <div className='articulo'>
         <p>
-           Estudiante de ultimo semestre de ingenieria de sofware en la universidad UniHorizonte 
-          con un tegnologo en informatica de la universidad Unigermana, 
-           tambien un tenico en bases de datos en el instituto incap, 
+           <span>Estudiante de ultimo semestre de ingenieria de sofware en la universidad UniHorizonte </span>
+           con un tegnologo en informatica de la universidad Unigermana, 
+           tambien un tenico en bases de datos en el instituto <br /> incap, 
            Gracias a mi trabajo tengo conocimientos en bases de datos como :oracle,
-           plsql,teradata,Azure,mysql.
+           <span>plsql,teradata,Azure,mysql. </span>
           </p>
 
         </div>
+
+        <section >
+          <form onSubmit={(e)=> {e.preventDefault();}}>
+            <label>
+              Nombre :
+              <input type="text" name="name" placeholder='ingrese su nombre' value={()=> {}} onChange={()=> {}}> </input>
+
+            </label>
+            <input type="submit" value="submit"></input>
+
+
+          </form>
+        </section>
+
+
         
 
       </div>
